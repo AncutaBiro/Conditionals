68874efaa1f836390d18ae76e72d6038417b478a
AncutaBiro <ancuta.biro@yahoo.com>
2020-09-29 21:03:29 +0300

    Add project files.

---
+++ Conditionals/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string color = Console.ReadLine();
+
+            int price = 0;
+            switch (color)
+            {
+                case "red":
+                    price = 10;
+                    break;
+                case "purple":
+                    goto case "blue";
+                case "blue":
+                    price += 1;
+                    goto case "green";
+                case "green":
+                    price += 2;
+                    goto case "white";
+                case "white":
+                    price += 6;
+                    break;
+                default:
+                    price = 5;
+                    break;
+            }
+            Console.WriteLine(price);
+            Console.Read();
+        }
+    }
+}
