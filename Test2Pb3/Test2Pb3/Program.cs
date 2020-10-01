943f02c5c5126802837b1625057fb09723cd9f80
AncutaBiro <ancuta.biro@yahoo.com>
2020-10-01 23:54:24 +0300

    starting the pb

--- Test2Pb3/Test2Pb3/Program.cs
+++ Test2Pb3/Test2Pb3/Program.cs
@@ -1,4 +1,30 @@
-﻿using System;
+﻿/*
+Convertește un număr dat, între 1 și 100, în cifre romane.
+I - 1
+V - 5
+X - 10
+L - 50
+C - 100
+Câteva exemple de comversie:
+4 - IV
+8 - VIII
+9 - IX
+15 - XV
+30 - XXX
+39 - XXXIX
+41 - XLI
+45 - XLV
+99 - XCIX
+Aplicația va citi de la consolă numărul și va afișa numărul scris cu cifre romane.
+
+Exemplu:
+Pentru datele de intrare:
+4
+La consolă se va afișa:
+IV
+ */
+
+using System;
 
 namespace Test2Pb3
 {
@@ -6,6 +32,8 @@ namespace Test2Pb3
     {
         static void Main(string[] args)
         {
+
+
             Console.WriteLine("Hello World!");
         }
     }
