285b94fb3e7cd4ce74c216432e61c0e9735d44b1
AncutaBiro <ancuta.biro@yahoo.com>
2020-10-02 18:01:55 +0300

    solution using if and recursion

--- Test2Pb3/Test2Pb3/Program.cs
+++ Test2Pb3/Test2Pb3/Program.cs
@@ -32,9 +32,30 @@ namespace Test2Pb3
     {
         static void Main(string[] args)
         {
+            string inputData = Console.ReadLine();
+            int n = Convert.ToInt32(inputData);
 
+            Console.WriteLine (intToRoman(n));
+            Console.Read();
+        }
+        static string intToRoman(int n)
+            {
+            if (n < 0) return "INVALID";
 
-            Console.WriteLine("Hello World!");
+            if (n == 0 ) return "";
+            if (n == 4) return "IV" + intToRoman(n - 4);
+            if (n == 9) return "IX" + intToRoman(n - 9);
+            if (n >= 40 && n < 50) return "XL" + intToRoman(n - 40);
+            if (n >= 90 && n < 100) return "XC" + intToRoman(n - 90);
+
+            if (n <= 3) return "I" + intToRoman(n - 1);
+            if (n < 10) return "V" + intToRoman(n - 5);
+            if (n < 40) return "X" + intToRoman(n - 10);
+            if (n < 100) return "L" + intToRoman(n - 50);
+            if (n == 100) return "C";
+            
+            return "INVALID";
+            }
         }
     }
-}
+
